568c56c0756eb06a5f2860a0ec41fc9c7ce0b0cc
lkonx <konxvon@hotmail.com>
2020-10-12 10:46:58 +0800

    初始化Vue-Ts项目

---
+++ vue-ts/src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="">
+        {{message}}
+    </div>
+</template>
+
+<script>
+    import {Vue, Component} from "vue-property-decorator";
+
+    @Component({
+        name: 'Home',
+        components: {}
+    })
+
+    export default class Home extends Vue {
+        message = 'home'
+    }
+</script>
+
+<style scoped>
+
+</style>
